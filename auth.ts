e345b72e559b238b267153f7b449760d8888ce39
GlendyT <glendytuyuc@gmail.com>
2024-02-29 20:53:47 -0600

    change

--- auth.ts
+++ auth.ts
@@ -1,8 +1,12 @@
 import NextAuth from "next-auth"
+import { UserRole } from "@prisma/client"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
+import { getUserById } from "@/data/user"
 import {db} from "@/lib/db"
 import authConfig from "./auth.config"
+
+
 
 export const {
     handlers: {GET, POST},
@@ -10,7 +14,63 @@ export const {
     signIn,
     signOut,
 } = NextAuth({
+    pages: {
+        signIn: "/auth/login",
+        error: "/auth/error",
+    },
+    events:{
+        async linkAccount({ user }){
+            await db.user.update({
+                where: { id: user.id},
+                data: { emailVerified: new Date()}
+            })
+        }
+    },
+    callbacks: {
+        async signIn({ user, account}) {
+
+            //ALLOW OAUTH WITHOUT EMAIL VERIFICATION
+            if (account?.provider !== "credentials") return true;
+
+            const existingUser = await getUserById(user.id);
+
+            // Prevent sign in without email verification
+            if (!existingUser?. emailVerified) return false;
+
+            // TODO: ADD 2FA CHECK
+            return true;
+        },
+        async session({ token, session}) {
+            if ( token.sub && session.user) {
+                session.user.id = token.sub;
+            }
+
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole; // as "ADMIN" | "USER"
+            }
+            return session;
+        },
+        async jwt ({ token }) {
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+
+            if (!existingUser) return token;
+
+            token.role = existingUser.role;
+            return token;
+        }
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt"},
     ...authConfig,
-})
+});
+
+/*        async signIn({ user}) {
+            const existingUser = await getUserById(user.id);
+
+            if (!existingUser || !existingUser.emailVerified) {
+                return false;
+            }
+            return true;
+        },*/

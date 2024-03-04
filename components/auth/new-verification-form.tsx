4c14c06276ac7e3ba3cb7acaab2db833b01e5db8
GlendyT <glendytuyuc@gmail.com>
2024-03-03 22:41:18 -0600

    cambios 3

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,20 +1,41 @@
 "use client"
 
+import { useCallback, useEffect, useState } from "react";
 import { BeatLoader } from "react-spinners";
 import { useSearchParams } from "next/navigation";
 
+import { newVerification } from "@/actions/new-verification";
 import { CardWrapper } from "@/components/auth/card-wrapper"
-import { useCallback, useEffect } from "react";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
+
 
 
 export const NewVerificationForm = () => {
+    const [error, setError ] = useState<string | undefined>();
+    const [success, setSuccess ] = useState<string | undefined>();
+
     const searchParams = useSearchParams();
 
     const token = searchParams.get("token");
 
     const onSubmit = useCallback(() => {
-        console.log(token)
-    }, [token]);
+        if (success || error ) return;
+
+        if (!token) {
+            setError("Missing token!");
+            return;
+        }
+
+        newVerification(token)
+        .then((data) => {
+            setSuccess(data.success);
+            setError(data.error);
+        })
+        .catch(() => {
+            setError("Something went wrong!")
+        })
+    }, [token, success, error]);
 
     useEffect(() => {
         onSubmit();
@@ -27,7 +48,14 @@ export const NewVerificationForm = () => {
          backButtonHref="/auth/login"
         >
             <div className="flex items-center w-full justify-center">
-                <BeatLoader />
+               {!success && !error && (
+                 <BeatLoader />
+               )}
+                <FormSuccess message={success}/>
+                {!success && (
+                 <FormError message={error}/> 
+                )}
+
             </div>
             
         </CardWrapper>
--- schemas/index.ts
+++ schemas/index.ts
@@ -2,6 +2,20 @@
 
 import * as z from "zod";
 
+export const NewPasswordSchema = z.object({
+    password:  z.string().min( 6, {
+        message: "Minimum of 6 characters required"
+    }),
+});
+
+
+export const ResetSchema = z.object({
+    email:  z.string().email({
+        message: "Email is required"
+    }),
+});
+
+
 export const LoginSchema = z.object({
     email:  z.string().email({
         message: "Email is required"
@@ -9,7 +23,10 @@ export const LoginSchema = z.object({
     password: z.string().min(1, {
         message: "Password is required"
     }),
+    code: z.optional(z.string()),
 });
+
+
 
 export const RegisterSchema = z.object({
     email:  z.string().email({
